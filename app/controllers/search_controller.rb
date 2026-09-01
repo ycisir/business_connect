bd3b554ca722bda0e25d10535b10f21792103d03
= <=>
2026-09-01 16:49:21 +0530

    add basic search and hero section

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,10 @@
+class SearchController < ApplicationController
+  def index
+    if params[:query].present?
+      @query = params[:query]
+    else
+      @query = "None"
+    end
+    
+  end
+end
